6f02ada45febf33ded7bcb051d861a5110a7342c
schmidt22-renan <schmidt.renan@escola.pr.gov.br>
2025-09-30 12:17:52 +0000

    schidt

--- vaquinha.c
+++ vaquinha.c
@@ -4,23 +4,29 @@ float totalArrecadado = 0;
 
 //procedimento: adiciona a contribuiçao de um participante
 void adicionaContribuicao(float valor){
-
-    printf("Contribuicao de R$ %.2f adicinada! tota: R$ %.2f",valor, totalArrecadado)
-
-
-
+    totalArrecadado += valor;
+    printf("Contribuição de R$ %.2f Adicionada! Total: R$ %.2f",valor, totalArrecadado)
 }
 
+
+
+
+
 //funcoa: calcula quantidada de carne por pessoa(0.5kg por pessoa)
- 
+ float calclaCarne(int qtdPessoa){
+  return qtdPessoa * 0.5;
+ }
 
 // Procedimento prara mostrar o resumo
 void mostraResumo(int qtdPessoas){
     float carne = calculaCarne(qtdPessoas)
-    float media = totalArrecadado
+    float media = totalArrecadado / qtdPessoas;
     printf("\n==== RESUMO CHURRASCO ====\n ");
-
     printf("Participantes: %d\n",qtdPessoas);
+    printf("Carne necesaria : %.2f KG\n", carne);
+    printf("Total arrecadado: R$ %.2f\n",totalArrecadado);
+    printf("Media por pessoa: R$ %.2f\n",media);
+    printf("===================================\n");
 }
 
 
@@ -32,6 +38,12 @@ int main(){
 
   printf("Digite o numero de participantes");
   scanf("%d",&pessoas);
+
+for (int i=1 ; i<= pessoas; i++){
+  printf("dDigite a contribuiçao do participante %d:R$", i);
+  adicionaContribuicao(valor);
+}
+
 mostraResumo(pessoas);
 
 
